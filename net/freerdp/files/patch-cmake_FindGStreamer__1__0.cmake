b697f4cfbfc60f7bb9d0350334e003b38f332545
Kyle Evans <kevans91@ksu.edu>
2016-08-03 23:34:38 -0500

    Initial stab at updating to an intermediate personal release of 2.0

---
+++ net/freerdp/files/patch-cmake_FindGStreamer__1__0.cmake
@@ -0,0 +1,12 @@
+--- cmake/FindGStreamer_1_0.cmake.orig	2016-07-22 13:34:49 UTC
++++ cmake/FindGStreamer_1_0.cmake
+@@ -78,7 +78,8 @@ endmacro()
+ # 1.1. Find headers and libraries
+ set(GLIB_ROOT_DIR ${GSTREAMER_1_0_ROOT_DIR})
+ find_package(Glib REQUIRED)
+-FIND_GSTREAMER_COMPONENT(GSTREAMER_1_0 gstreamer-1.0 gstreamer-1.0)
++pkg_check_modules (GSTREAMER_1_0 gstreamer-1.0)
++#FIND_GSTREAMER_COMPONENT(GSTREAMER_1_0 gstreamer-1.0 gstreamer-1.0)
+ FIND_GSTREAMER_COMPONENT(GSTREAMER_1_0_BASE gstreamer-base-1.0 gstbase-1.0)
+ 
+ # 1.2. Check Gstreamer version
